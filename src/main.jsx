9c29b0579941ffd19616412cac12144cea7a3a0d
Sebastian Echeverri Varela <sebastianecheverrivarela@Sebastians-MacBook-Pro.local>
2025-04-29 02:51:37 -0500

    Final

--- src/main.jsx
+++ src/main.jsx
@@ -22,15 +22,4 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', () => {
-//     navigator.serviceWorker.register('/service-worker.js')
-//       .then(registration => {
-//         console.log('Service Worker registrado:', registration);
-//       })
-//       .catch(registrationError => {
-//         console.log('Service Worker error:', registrationError);
-//       });
-//   });
-// }
 
